f217b46469092cdaff1a9dffd9b8af3f7b87f802
markpeach59 <mark.peach@frontbacktech.com>
2026-01-30 09:44:59 +0000

    eslint issue

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -20,7 +20,7 @@ function setJwt(jwt) {
   axios.defaults.headers.common["x-auth-token"] = jwt;
 }
 
-export default {
+const httpService = {
   get: axios.get,
   post: axios.post,
   put: axios.put,
@@ -28,3 +28,5 @@ export default {
   delete: axios.delete,
   setJwt
 };
+
+export default httpService;
